4180fcce55b3ae4d4d880b19b4eb4deb527136fe
Anthony N <anythony20@gmail.com>
2024-05-28 04:48:42 -0400

    add: row wrapping

--- multi_modal_tokenizers/tokenizers/mixed_modal_tokenizer.py
+++ multi_modal_tokenizers/tokenizers/mixed_modal_tokenizer.py
@@ -1,6 +1,15 @@
 import torch
 from warnings import warn
 
+def flatten_2d(lst):
+    return [item for sublist in lst for item in sublist]
+
+def split_into_chunks(lst, n):
+    if n <= 0:
+        raise ValueError("Chunk size must be greater than 0")
+    
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+
 class MixedModalTokenizer:
     def __init__(
             self,
@@ -9,11 +18,11 @@ class MixedModalTokenizer:
             image_placement_tag = "<image>",
             image_start_tag = "<image_start>",
             image_end_tag = "<image_end>",
-            device='cpu'
+            wrap_rows=False
         ):
         self.text_tokenizer = text_tokenizer
         self.image_tokenizer = image_tokenizer
-        self.device = device
+        self.wrap_rows = wrap_rows
 
         # Calculate tokens per image based on the tokenizer's properties
         self.num_tokens_per_image = (image_tokenizer.image_dim // image_tokenizer.downscale_factor) ** 2
@@ -26,18 +35,37 @@ class MixedModalTokenizer:
                 image_end_tag
             ]
         }
+
+        if self.wrap_rows:
+            for i in range(self.image_tokenizer.image_dim):
+                new_tokens['additional_special_tokens'] += [f"<row_{i}_start>", f"<row_{i}_end>"]
+
         text_tokenizer.add_special_tokens(new_tokens)
         self.image_placement_id, self.image_start_id, self.image_end_id = [
-            text_tokenizer.convert_tokens_to_ids(token) for token in new_tokens['additional_special_tokens']
+            text_tokenizer.convert_tokens_to_ids(token) for token in new_tokens['additional_special_tokens'][:3]
         ]
         self.image_id_offset = len(text_tokenizer)
+
+        if self.wrap_rows:
+            self.row_wrap_ids = [ text_tokenizer.convert_tokens_to_ids(f"<row_{i}_start>") for i in range(self.image_tokenizer.image_dim) ]
+            self.row_wrap_ids += [ text_tokenizer.convert_tokens_to_ids(f"<row_{i}_end>") for i in range(self.image_tokenizer.image_dim) ]
+        else:
+            self.row_wrap_ids = []
 
     def set_image_dim(self, dim):
         self.image_tokenizer.image_dim = dim
         self.num_tokens_per_image = (self.image_tokenizer.image_dim // self.image_tokenizer.downscale_factor) ** 2
 
+        if self.wrap_rows:
+            new_tokens = { 'additional_special_tokens': [] }
+            for i in range(self.image_tokenizer.image_dim):
+                new_tokens['additional_special_tokens'] += [f"<row_{i}_start>", f"<row_{i}_end>"]
+            self.text_tokenizer.add_special_tokens(new_tokens)
+            self.image_id_offset = len(self.text_tokenizer)
+
     def __len__(self):
-        return len(self.text_tokenizer) + len(self.image_tokenizer)
+        result = len(self.text_tokenizer) + len(self.image_tokenizer)
+        return result
 
     def encode(self, text="", images=[]):
         encoded_text = self.text_tokenizer.encode(text)
@@ -46,10 +74,21 @@ class MixedModalTokenizer:
 
         if not images:
             return encoded_text
-
+        
+        def _encode_image(img):
+            result = self.image_tokenizer.encode(img).to('cpu').tolist()
+            if self.wrap_rows:
+                result = split_into_chunks(result, self.image_tokenizer.image_dim // self.image_tokenizer.downscale_factor)
+                for i in range(len(result)):
+                    row_start_id = self.text_tokenizer.convert_tokens_to_ids(f"<row_{i}_start>")
+                    row_start_end = self.text_tokenizer.convert_tokens_to_ids(f"<row_{i}_end>")
+                    result[i] = [row_start_id] + result[i] + [row_start_end]
+                result = flatten_2d(result)
+            return result
+        
         # Encode images and adjust token IDs with offset
         encoded_images = [
-            [x + self.image_id_offset for x in self.image_tokenizer.encode(img).to('cpu').tolist()]
+            [x + self.image_id_offset for x in _encode_image(img)]
             for img in images
         ]
 
@@ -79,7 +118,7 @@ class MixedModalTokenizer:
                 if not suppress_warnings:
                     warn(f"Image token sequence shorter than expected ({self.num_tokens_per_image}). Padding.")
                 buf += [self.image_id_offset] * (self.num_tokens_per_image - len(buf))
-            images.append(self.image_tokenizer.decode(torch.tensor(buf, device=self.device)))
+            images.append(self.image_tokenizer.decode(torch.tensor(buf)))
             buf = []
 
         # Process the encoded token IDs to extract text and images
@@ -93,13 +132,14 @@ class MixedModalTokenizer:
                 process_image_buffer()
             elif scanning_image:
                 image_id = id - self.image_id_offset
+                if image_id in self.row_wrap_ids: continue
                 if image_id >= 0:
                     buf.append(image_id)
                 elif not suppress_warnings:
                     warn(f"Invalid token id ({image_id}) within image context. Ignoring.")
         
         # Remove image related IDs and decode text
-        filtered_ids = [id for id in input_ids if id < self.image_id_offset and id not in {self.image_placement_id, self.image_start_id, self.image_end_id}]
+        filtered_ids = [id for id in input_ids if id < self.image_id_offset and id not in {self.image_placement_id, self.image_start_id, self.image_end_id} and id not in self.row_wrap_ids]
         decoded_text = self.text_tokenizer.decode(filtered_ids)
 
         return decoded_text, images
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -51,3 +51,24 @@ def test_mixed_modal_tokenizer_encode_decode(text_tokenizer, dalle_tokenizer):
     assert isinstance(decoded_text, str), "Decoded text should be a string"
     assert all(isinstance(img, Image.Image) for img in decoded_images), "Decoded images should be PIL Images"
     assert len(mixed_tokenizer) == len(text_tokenizer) + len(dalle_tokenizer), "__len__ Mismatch"
+
+def test_mixed_modal_tokenizer_encode_decode_wrap(text_tokenizer, dalle_tokenizer):
+    mixed_tokenizer = MixedModalTokenizer(
+        text_tokenizer=text_tokenizer,
+        image_tokenizer=dalle_tokenizer,
+        wrap_rows=True
+    )
+
+    text = "This is an example with <image> in the middle."
+    img_url = 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iKIWgaiJUtss/v2/1000x-1.jpg'
+    image = download_image(img_url)
+
+    # Encode the text and image
+    encoded = mixed_tokenizer.encode(text=text, images=[image])
+    assert isinstance(encoded, list), "Encoded result should be a list"
+
+    # Decode the sequence back to text and image
+    decoded_text, decoded_images = mixed_tokenizer.decode(encoded)
+    assert isinstance(decoded_text, str), "Decoded text should be a string"
+    assert all(isinstance(img, Image.Image) for img in decoded_images), "Decoded images should be PIL Images"
+    assert len(mixed_tokenizer) == len(text_tokenizer) + len(dalle_tokenizer), "__len__ Mismatch"
